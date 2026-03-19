051c3e23a2a2fa533889dafb6a586c004f2927dc
Даниил <sapunov.daniil13@gmail.com>
2026-03-19 22:01:42 +0500

    В файле «message.php», прописать проверку токена, созданного сервером, и отправленного пользователем:

--- ajax/message.php
+++ ajax/message.php
@@ -3,8 +3,12 @@
 	include("../settings/connect_datebase.php");
 
     $IdUser = $_SESSION['user'];
+    $Token = $_POST["Token"];
     $Message = $_POST["Message"];
     $IdPost = $_POST["IdPost"];
+
+    if($Token != $_SESSION["CSRF"])
+        exit;
 
     $mysqli->query("INSERT INTO `comments`(`IdUser`, `IdPost`, `Messages`) VALUES ({$IdUser}, {$IdPost}, '{$Message}');");
 ?>
